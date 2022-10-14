50f7c6adc5e359c605ed319d0f62d64d3b7d74f4
HS <2992746362@qq.com>
2022-10-14 16:48:21 +0800

    1648

--- MyBatis_plus_demo/src/test/java/com/css/Tests.java
+++ MyBatis_plus_demo/src/test/java/com/css/Tests.java
@@ -36,4 +36,37 @@ class Tests {
         List<User> list = userMapper.selectList(queryWrapper).subList(0,3);
         list.forEach(System.out::println);
     }
+
+    //查看允许玩家查询⾃⼰前后共10⼈的分数和名次,name为玩家的名称
+    @Test
+    public void test02(String name){
+        //查出玩家自己的信息
+        name = "tom";
+        QueryWrapper<User> queryWrapper1 = new QueryWrapper<>();
+        queryWrapper1.eq("name",name);
+        List<User> list1 = userMapper.selectList(queryWrapper1);
+        //向前排序
+        QueryWrapper<User> queryWrapper2 = new QueryWrapper<>();
+        queryWrapper2.ge("score",list1.get(0).getScore())
+                .ge("time",list1.get(0).getTime())
+                .orderByDesc("score").orderByDesc("time");
+        List<User> list2 = userMapper.selectList(queryWrapper2);
+        if(list2.size()>= 10){
+            List<User> list22 = list2.subList(list2.size()-10, list2.size());
+            list22.forEach(System.out::println);
+        }else {
+            list2.forEach(System.out::println);
+            QueryWrapper<User> queryWrapper3 = new QueryWrapper<>();
+            queryWrapper3.le("score",list1.get(0).getScore())
+                    .le("time",list1.get(0).getTime())
+                    .orderByDesc("score").orderByDesc("time");
+            List<User> list3 = userMapper.selectList(queryWrapper3);
+            if(list3.size()>= (10-list2.size())){
+                List<User> list33 = list2.subList(0, (10-list2.size()));
+                list33.forEach(System.out::println);
+            }else {
+                list3.forEach(System.out::println);
+            }
+        }
+    }
 }
